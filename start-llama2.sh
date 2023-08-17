a697ed6d2e891721211aa6131ec6272d478b923f
Michael Schmid <michael@amazee.com>
2023-08-17 10:38:09 -0600

    prep to have a cpu and gpu version

--- start-llama2.sh
+++ start-llama2.sh
@@ -5,4 +5,4 @@ ulimit -l unlimited
 echo "Model: ${MODEL}"
 
 python3 hug_model.py -s ${MODEL} -f "q5_1"
-python3 -B -m llama_cpp.server --model /data/model.bin --n_gpu_layers=43
+python3 -B -m llama_cpp.server --model /data/model.bin ${LLAMA_CPP_ARGS}
